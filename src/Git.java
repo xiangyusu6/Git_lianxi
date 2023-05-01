30c1e66a687d56070c3e61a03889186c532335e7
xiangyusu <waed8d5@163.com>
2023-05-01 15:01:35 +0800

    Initial commit

---
+++ src/Git.java
@@ -0,0 +1,7 @@
+public class Git {
+    public static void main(String[] args) {
+        System.out.println("Git实在是太方便了");
+        System.out.println("使用Git能帮助我们获得更多的资料");
+        System.out.println("Git实在是太厉害了");
+    }
+}
